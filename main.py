80a09220c23f2fef1c8293ee06730ebfd7d66812
mto3737 <64856860+mto3737@users.noreply.github.com>
2021-06-29 19:13:53 +0200

    anpassungen

--- main.py
+++ main.py
@@ -4,25 +4,85 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+
+from GUIGAN_main import build_generator
+from GUIGAN_test import build_result_uis
+from get_style_emb import get_style_emb
+from modelGenerator.load_data import load_data
+from modelGenerator.load_subtrees import load_subtrees
+from modelGenerator.train_siamese_net import train_siamese
 from subtreeGenerator.save_subtree_info import save_subtree_info
+import tkinter as tk
+
+json_rico = r'.\folders\Rico\jsons'  # Rico Json Dateien
+gui_dir_rico = r'.\folders\Rico\gui'  # Rico
+gui_information_dir = r'.\folders\gui_informations'
+control_elements_id_dir = r'.\folders\gui_control_elements'
+cutted_ui_elements = r'.\folders\cutted_ui_elements'
+cutted_resized_ui_elements = r'.\folders\cutted_resized_ui_elements'
+data_dir = r'.\folders\data'
+models_torch_dir = r'.\folders\models_torch_dir'
+app_details_csv = r'.\folders\app_details.csv'
+categories_app_emb = r'.\folders\categories_app_emb'
+models_dir = r'.\folders\models'
+results_dir = r'.\folders\results'
+results_pre_dir = r'.\folders\results_pre'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def build_ui():
+    window = tk.Tk()
 
+    window.geometry("300x200+10+20")
 
-# Press the green button in the gutter to run the script.
+    # Textausgabe erzeugen
+    label1 = tk.Label(window, text="Hallo Welt")
+    label1.pack()
+
+    schaltf1 = tk.Button(window, text="1. UIs zerschneiden.", command = save_subtree)
+    schaltf1.pack()
+
+    load_data_button = tk.Button(window, text="1. Lade UI Daten.", command = load_data_for_model)
+    load_data_button.pack()
+
+    load_subtrees = tk.Button(window, text="Model genieren", command = generateModel)
+    load_subtrees.pack()
+
+    build_result_uis_button = tk.Button(window, text="generate_categories", command = generate_categories)
+    build_result_uis_button.pack()
+
+    build_result_uis_button = tk.Button(window, text="generate_generators", command = generate_generators)
+    build_result_uis_button.pack()
+
+    build_result_uis_button = tk.Button(window, text="build_result_uis_button", command = generate_uis)
+    build_result_uis_button.pack()
+
+    window.mainloop()
+
+def save_subtree():
+    save_subtree_info(json_rico, gui_dir_rico, gui_information_dir, control_elements_id_dir, cutted_ui_elements,
+                      cutted_resized_ui_elements)
+
+def load_data_for_model():
+    load_data(cutted_resized_ui_elements,data_dir)
+    load_subtrees(cutted_resized_ui_elements,data_dir)
+
+def generateModel():
+    train_siamese(cutted_resized_ui_elements,data_dir,models_torch_dir)
+
+def generate_generators():
+    build_generator(app_details_csv,models_dir,gui_information_dir,control_elements_id_dir,categories_app_emb,results_dir,results_pre_dir,cutted_ui_elements,cutted_resized_ui_elements)
+
+def generate_uis():
+    build_result_uis(app_details_csv,models_dir,gui_information_dir,control_elements_id_dir,categories_app_emb,results_dir,results_pre_dir,cutted_ui_elements,cutted_resized_ui_elements)
+
+def generate_categories():
+    get_style_emb(models_torch_dir,app_details_csv,categories_app_emb,cutted_ui_elements,cutted_resized_ui_elements)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-    json_rico = r'.\folders\Rico\jsons'  # Rico Json Dateien
-    gui_dir_rico = r'.\folders\Rico\gui'  # Rico
-    gui_information_dir = r'.\folders\gui_informations'
-    control_elements_id_dir = r'.\folders\gui_control_elements'
-    cutted_ui_elements = r'.\folders\cutted_ui_elements'
-    cutted_resized_ui_elements = r'.\folders\cutted_resized_ui_elements'
-
     if not os.path.exists(json_rico):
         os.makedirs(json_rico)
 
@@ -41,8 +101,13 @@ if __name__ == '__main__':
     if not os.path.exists(cutted_resized_ui_elements):
         os.makedirs(cutted_resized_ui_elements)
 
+    if not os.path.exists(categories_app_emb):
+        os.makedirs(categories_app_emb)
 
-    save_subtree_info(json_rico, gui_dir_rico, gui_information_dir, control_elements_id_dir, cutted_ui_elements,
-                      cutted_resized_ui_elements)
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if not os.path.exists(results_pre_dir):
+        os.makedirs(results_pre_dir)
+
+    build_ui()
---
+++ modelGenerator/load_data.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+
+import os,random,csv
+import numpy as np
+
+def load_dataset(ui_path):
+    count = 0
+    ui_dictionary = {}
+    for app in os.listdir(ui_path):
+        app_path = os.path.join(ui_path, app)
+        current_app = []
+        for character in os.listdir(app_path):
+            character_path = os.path.join(app_path, character)
+            current_app.append(character_path)
+            count += 1
+        ui_dictionary[app] = current_app
+    print('Total_GUI_num: ', count)
+    return ui_dictionary
+
+def split_valid_datasets(data, r1 = 0.9, r2 = 0.94):
+    available_apps = list(data.keys())
+    number_of_apps = len(available_apps)
+    train_valid_apps = []
+    train_apps = []
+    validation_apps = []
+    test_apps = []
+    print('number_of_apps', number_of_apps)
+    train_indexes = random.sample(range(0, number_of_apps - 1), int(r1 * number_of_apps))
+    train_indexes.sort(reverse=True)
+    for index in train_indexes:
+        train_valid_apps.append(available_apps[index])
+        available_apps.pop(index)
+    test_apps = available_apps
+    
+    number = len(train_valid_apps)
+    validation_index = random.sample(range(0, number - 1), int(r2 * number))
+    validation_index.sort(reverse=True)
+    for index in validation_index:
+        train_apps.append(train_valid_apps[index])
+        train_valid_apps.pop(index)
+    validation_apps = train_valid_apps    
+    return train_apps, validation_apps, test_apps
+
+def write_file(path_file_name,_str):
+    if not os.path.exists(path_file_name):
+        with open(path_file_name, "w") as f:
+            print(f)
+    with open(path_file_name, "a") as f:
+        f.write(_str)
+        
+def read_valid_file(_file):
+    with open(_file, 'r') as f:
+        apps = f.read()
+    train_apps, valid_apps, test_apps = apps.split(';')[0],apps.split(';')[1],apps.split(';')[2]
+    train_apps = train_apps.split(','); train_apps.pop(-1)
+    valid_apps = valid_apps.split(','); valid_apps.pop(-1)
+    test_apps = test_apps.split(','); test_apps.pop(-1)
+    return train_apps, valid_apps, test_apps
+
+def get_random_ui(dictionary,available_apps,n_min=1,n_max=5):
+    selected_uis = []
+    print('available_apps_nums: ', len(available_apps))
+    for i in range(len(available_apps)):
+        current_app = available_apps[i]
+        current_app_dirs = dictionary[current_app]
+        print('\ncurrent_app: ', current_app)
+        print('available_uis_nums: ', len(current_app_dirs))
+        if len(current_app_dirs) <n_min:
+            continue
+        arr = np.arange(len(current_app_dirs))
+        np.random.shuffle(arr)
+        print('random select: ', arr)
+        arr = arr[:n_max]
+        print('random select cut: ', arr)
+        for j in range(int(len(arr))):
+            selected_uis.append(current_app_dirs[arr[j]])
+            print('selected ui:', current_app_dirs[arr[j]])
+    return selected_uis
+  
+# read UI_pair
+def read_file(ui_file): # "；" is used to split intra pair and inter pair
+    _uis = []
+    with open(ui_file, 'r') as f:
+        for line in f.readlines():
+            linelist = line.split(',')
+            _uis.append(linelist)
+    return _uis[0][:-1]
+
+def get_s_app(file_csv, cd, app=True):
+    categories = []
+    cv_file = csv.reader(open(file_csv,'r', encoding='UTF-8'))
+    for stu in cv_file:
+        categories.append([stu[0].strip(),stu[2].strip()])
+        
+    categories1 = categories[1:]
+    p5_apps = []
+    for a in os.listdir(cd):
+        app_dir = os.path.join(cd,a)
+        a_ui_num = 0
+        for b in os.listdir(app_dir):
+            a_ui_num += 1
+        p5_apps.append([a.strip(), a_ui_num])
+    
+    categories3 = []
+    for c in categories1:
+        if c[0] in [p[0] for p in p5_apps]:
+            c3 = [d for d in p5_apps if d[0]==c[0]][0]
+            categories3.append([c[0], c[1], c3[1]])    
+    if app:
+        p5_c_l = []
+        p5_c_d = {}
+        for c in categories3:
+            if c[1] not in p5_c_l:
+                p5_c_l.append(c[1])
+                p5_c_d[c[1]] = [c[0]]
+            else:
+                p5_c_d[c[1]] += [c[0]]                
+        return p5_c_l, p5_c_d
+    else:
+        p5_c_l1 = []
+        p5_c_d1 = {}
+        for c in categories3:
+            if c[1] not in p5_c_l1:
+                p5_c_l1.append(c[1])
+                p5_c_d1[c[1]] = int(c[2])
+            else:
+                p5_c_d1[c[1]] += int(c[2])
+                
+        p5_c_ds1 = sorted(p5_c_d1.items(), key = lambda x:x[1])
+        return p5_c_l1, p5_c_ds1
+
+#-------------------main start-----------------------------------
+def load_data(cutted_resized_ui_elements,data_dir):
+
+    ui_dictionary = load_dataset(cutted_resized_ui_elements)
+
+    # write the selected apps to the file
+    _file = data_dir + '\data.txt'
+    train_apps, valid_apps, test_apps = split_valid_datasets(ui_dictionary)
+    for app in train_apps:
+        write_file(_file, app + ',')
+    write_file(_file, ';')
+    for app in valid_apps:
+        write_file(_file, app + ',')
+    write_file(_file, ';')
+    for app in test_apps:
+        write_file(_file, app + ',')
+
+    # read the selected apps from the file
+    train_apps, valid_apps, test_apps = read_valid_file(_file)
+
+    _train_file = data_dir + '\\train_ui.txt'
+    _valid_file = data_dir + '\\valid_ui.txt'
+    _test_file = data_dir +  '\\test_ui.txt'
+
+    train_uis = get_random_ui(ui_dictionary, train_apps)
+    valid_uis = get_random_ui(ui_dictionary, valid_apps)
+    test_uis = get_random_ui(ui_dictionary, test_apps)
+
+    # write the pair data to file
+    for ui in train_uis:
+        write_file(_train_file, ui + ',')
+    for ui in valid_uis:
+        write_file(_valid_file, ui + ',')
+    for ui in test_uis:
+        write_file(_test_file, ui + ',')
+    # ------------------------------------------------------#
+    #    # read the pair data from file
+    train_uis = read_file(_train_file)  # 6368
+    
