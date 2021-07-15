70771f6a2b2d170984bcceed391bf9af539e1c4c
mto3737 <64856860+mto3737@users.noreply.github.com>
2021-07-15 12:55:06 +0200

    parser

--- parser/main.py
+++ parser/main.py
@@ -2,11 +2,14 @@ import json
 
 class ViewObj:
 
-    def __init__(self, id, index, space):
+    def __init__(self, id, index, space,android_class):
         self.id = id
         self.index = index
         self.spaces = space
         self.children = []
+        self.visible_to_user = True
+        self.android_class = android_class
+        self.
 
 
 def load_file(name):
@@ -35,33 +38,33 @@ def load_file(name):
     lineIndex = 0
     spaces = 0
     for line in lines:
-        # clean String
-        line = line.replace('\x00', '')
-        line = line.replace('\0', '')
-        line = line.replace('\0', '')
-        line = line.replace('z\4', '')
+
+        line = clearString(line)
 
         id = line.lstrip().split()[0].split("@")[1]  ## ID of View
+        android_class = line.lstrip().split()[0].split("@")[0]  ## class of View
+        print(android_class)
         lineIndex += 1
 
         for char in line:
             if char.isspace() is False:
                 print("ID: " + str(id) + " Spaces: " + str(spaces))
-                listOfViewObj.append(ViewObj(id, lineIndex, spaces))
+                listOfViewObj.append(ViewObj(id, lineIndex, spaces,android_class))
                 spaces = 0
                 break
 
             spaces += 1
 
     listOfViewObj = matchChildren(listOfViewObj)
+
+    toJson(listOfViewObj)
+
+def toJson(listOfViewObj):
     jsonStr = json.dumps(listOfViewObj[0].__dict__)
+
+    jsonStr = jsonStr.replace("visible_to_user","visible-to-user")
+    jsonStr = jsonStr.replace("android_class","class")
     print(jsonStr)
-
-    lines_splited = []
-    # Split Eigenschaften
-    for line in lines:
-        lines_splited.append(line.split())
-
 
 def matchChildren(objects):
     for i in range(len(objects)):
@@ -82,6 +85,15 @@ def getParent(objects, index, spaces):
         else:
             index -= 1
 
+
+def clearString(string):
+    # clean String
+    string = string.replace('\x00', '')
+    string = string.replace('\0', '')
+    string = string.replace('\0', '')
+    string = string.replace('z\4', '')
+    string = string.replace('x', '')
+    return string
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
