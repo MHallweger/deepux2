d01b428ee7511305c7611dffc9291f2913b174db
mto3737 <64856860+mto3737@users.noreply.github.com>
2021-07-15 15:32:39 +0200

    test bounds

--- parser/main.py
+++ parser/main.py
@@ -2,16 +2,18 @@ import json
 
 class ViewObj:
 
-    def __init__(self, id, index, space,android_class,layout_height,layout_width,location_on_screen_x,location_on_screen_y):
+    def __init__(self, id,resourceId, index, space,android_class,layout_height,layout_width,location_on_screen_x,location_on_screen_y,visibility):
         self.id = id
+        self.resourceId = resourceId
         self.index = index
         self.spaces = space
-        self.visible_to_user = True
+        self.visible_to_user = visibility
         self.android_class = android_class
         self.layout_height = layout_height
         self.layout_width = layout_width
         self.location_on_screen_x = location_on_screen_x
         self.location_on_screen_y = location_on_screen_y
+        self.bounds = [location_on_screen_x,location_on_screen_y,layout_width,layout_height]
         self.children = []
 
 
@@ -44,13 +46,14 @@ def load_file(name):
     for line in lines:
 
         line = clearString(line)
-
         id = line.lstrip().split()[0].split("@")[1]  ## ID of View
         android_class = line.lstrip().split()[0].split("@")[0]
         layout_height = -1
         layout_width = -1
         getLocationOnScreen_x = -1
         getLocationOnScreen_y = -1
+        getVisibility = False
+        resourceId = -1
         for attributes in line.lstrip().split():
             if 'getHeight' in attributes:
                 layout_height = attributes.split("=")[1].split(",")[1]
@@ -60,6 +63,16 @@ def load_file(name):
                 getLocationOnScreen_x = attributes.split("=")[1].split(",")[1]
             elif 'getLocationOnScreen_y' in attributes:
                 getLocationOnScreen_y = attributes.split("=")[1].split(",")[1]
+            elif 'getVisibility' in attributes:
+                getVisibility = attributes.split("=")[1].split(",")[1]
+                if getVisibility == 'VISIBLE':
+                    getVisibility = True
+                else:
+                    getVisibility = False
+            elif 'mID' in attributes:
+                resourceId = attributes.split("=")[1].split(",")[1]
+
+
 
 
         print(android_class)
@@ -68,7 +81,16 @@ def load_file(name):
         for char in line:
             if char.isspace() is False:
                 print("ID: " + str(id) + " Spaces: " + str(spaces))
-                listOfViewObj.append(ViewObj(id, lineIndex, spaces,android_class,layout_height,layout_width,getLocationOnScreen_x,getLocationOnScreen_y))
+                listOfViewObj.append(ViewObj(id,
+                                             resourceId,
+                                             lineIndex,
+                                             spaces,
+                                             android_class,
+                                             layout_height,
+                                             layout_width,
+                                             getLocationOnScreen_x,
+                                             getLocationOnScreen_y,
+                                             getVisibility))
                 spaces = 0
                 break
 
@@ -112,8 +134,12 @@ def clearString(string):
     string = string.replace('\0', '')
     string = string.replace('z\4', '')
     string = string.replace('x', '')
+    string = string.replace('4', '')
+    string = string.replace('', '')
+    string = string.replace('', '')
+
     return string
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    load_file('test6.li')
+    load_file('test7.li')
